56fc9690699763aa255236b7e78af7b2e33d4e5e
alejostaffieri <alejo.staffieri@gmail.com>
2026-06-05 19:10:38 -0300

    fix: corregir URL (Maps Platform usa locations/global)

--- app/api/sugerir-asignacion/route.ts
+++ app/api/sugerir-asignacion/route.ts
@@ -202,7 +202,7 @@ async function sugerirConRouteOptimization(
     considerRoadTraffic: false,
   }
 
-  const url = `https://routeoptimization.googleapis.com/v1/projects/${projectId}/:optimizeTours`
+  const url = `https://routeoptimization.googleapis.com/v1/projects/${projectId}/locations/global:optimizeTours`
   const response = await fetch(url, {
     method: 'POST',
     headers: {

899ccdc6adfb76b320f3c2a7eef4f9e870be5053
alejostaffieri <alejo.staffieri@gmail.com>
2026-06-05 19:06:47 -0300

    fix: probar URL sin especificar región

--- app/api/sugerir-asignacion/route.ts
+++ app/api/sugerir-asignacion/route.ts
@@ -202,7 +202,7 @@ async function sugerirConRouteOptimization(
     considerRoadTraffic: false,
   }
 
-  const url = `https://routeoptimization.googleapis.com/v1/projects/${projectId}/locations/europe-west1:optimizeTours`
+  const url = `https://routeoptimization.googleapis.com/v1/projects/${projectId}/:optimizeTours`
   const response = await fetch(url, {
     method: 'POST',
     headers: {
